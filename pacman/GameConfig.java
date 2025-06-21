3a87fbe63d3f067d461012bfdf7998c80be0badb
cebochamane <cebosbongachamane@gmail.com>
2025-06-21 19:26:58 +0200

    Added GameConfig.java

---
+++ pacman/GameConfig.java
@@ -0,0 +1,32 @@
+public class GameConfig {
+    public static final int ROW_COUNT = 21;
+    public static final int COLUMN_COUNT = 19;
+    public static final int TILE_SIZE = 32;
+    public static final int BOARD_WIDTH = COLUMN_COUNT * TILE_SIZE;
+    public static final int BOARD_HEIGHT = ROW_COUNT * TILE_SIZE;
+    public static final char[] DIRECTIONS = { 'U', 'D', 'L', 'R' };
+
+    public static final String[] TILE_MAP = {
+            "XXXXXXXXXXXXXXXXXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X        C       X",
+            "X XX X XXXXX X XX X",
+            "X    X       X    X",
+            "XXXX XXXX XXXX XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXrXX X XXXX",
+            "O       bpo       O",
+            "XXXX X XXXXX X XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXXXX X XXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X  X     P     X  X",
+            "XX X X XXXXX X X XX",
+            "X    X   X   X    X",
+            "X XXXXXX X XXXXXX X",
+            "X        C       X",
+            "XXXXXXXXXXXXXXXXXXX"
+    };
+}
