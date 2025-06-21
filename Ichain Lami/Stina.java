07d13e8f04c0556d52a8fa8ed5f43342046a51de
cebochamane <cebosbongachamane@gmail.com>
2025-06-21 15:45:03 +0200

    Added addtranscation functionality to block

--- Ichain Lami/Stina.java
+++ Ichain Lami/Stina.java
@@ -29,12 +29,29 @@ public class Stina {
 
     public void mineBlock(int difficulty) {
         merkleRoot = StringUtil.getMerkleRoot(transactions);
-        String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
+        String target = StringUtil.getDifficultyString(difficulty); // Create a string with difficulty * "0"
         while (!hash.substring(0, difficulty).equals(target)) {
             nonce++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
+    }
+
+    // Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        // process transaction and check if valid, unless block is genesis block then
+        // ignore.
+        if (transaction == null)
+            return false;
+        if ((prevHash != "0")) {
+            if ((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 
 }
