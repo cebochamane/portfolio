264c831b7bf384a1d3a04a4e640d6402a747bafe
cebochamane <cebosbongachamane@gmail.com>
2025-06-25 04:53:41 +0200

    Updated Transaction.java

--- Ichain Lami/Transaction.java
+++ Ichain Lami/Transaction.java
@@ -1,106 +1,158 @@
 import java.security.*;
 import java.util.ArrayList;
 
+/**
+ * Represents a financial transaction between wallets
+ */
 public class Transaction {
 
-    public String transactionId; // this is also the hash of the transaction.
-    public PublicKey sender; // senders address/public key.
-    public PublicKey recipient; // Recipients address/public key.
-    public float value;
-    public byte[] signature; // this is to prevent anybody else from spending funds in our wallet.
+    // Transaction metadata
+    public String transactionId; // SHA-256 hash of transaction
+    public PublicKey sender; // Sender's public key
+    public PublicKey recipient; // Recipient's public key
+    public float value; // Amount being transferred
+    public byte[] signature; // ECDSA signature
 
+    // Transaction inputs (UTXOs being spent)
     public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+    // Transaction outputs (new UTXOs being created)
     public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
 
-    private static int sequence = 0; // a rough count of how many transactions have been generated.
+    // Counter to ensure unique transaction IDs
+    private static int sequence = 0;
 
-    // Constructor:
+    /**
+     * Constructor
+     * 
+     * @param from   Sender's public key
+     * @param to     Recipient's public key
+     * @param value  Amount to transfer
+     * @param inputs UTXOs being spent (null for coinbase transactions)
+     */
     public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
         this.sender = from;
         this.recipient = to;
         this.value = value;
-        this.inputs = inputs;
+        this.inputs = (inputs != null) ? inputs : new ArrayList<TransactionInput>();
     }
 
-    // This Calculates the transaction hash (which will be used as its Id)
-    private String calulateHash() {
-        sequence++; // increase the sequence to avoid 2 identical transactions having the same hash
-        return StringUtil.turnIntoUnrecognizableGibberish(
+    /**
+     * Calculates transaction hash (used as ID)
+     * 
+     * @return SHA-256 hash of transaction data
+     */
+    private String calculateHash() {
+        sequence++; // Ensure unique hash even for identical transactions
+        return StringUtil.applySha256(
                 StringUtil.getStringFromKey(sender) +
                         StringUtil.getStringFromKey(recipient) +
-                        Float.toString(value) + sequence);
+                        Float.toString(value) +
+                        sequence);
     }
 
-    // Signs all the data we dont wish to be tampered with.
+    /**
+     * Generates ECDSA signature for transaction
+     * 
+     * @param privateKey Sender's private key
+     */
     public void generateSignature(PrivateKey privateKey) {
-        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient)
-                + Float.toString(value);
+        // Create signing data from immutable transaction fields
+        String data = StringUtil.getStringFromKey(sender) +
+                StringUtil.getStringFromKey(recipient) +
+                Float.toString(value);
         signature = StringUtil.applyECDSASig(privateKey, data);
     }
 
-    // Verifies the data we signed hasnt been tampered with
+    /**
+     * Verifies transaction signature
+     * 
+     * @return true if signature is valid
+     */
     public boolean verifySignature() {
-        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient)
-                + Float.toString(value);
+        // Recreate signing data
+        String data = StringUtil.getStringFromKey(sender) +
+                StringUtil.getStringFromKey(recipient) +
+                Float.toString(value);
         return StringUtil.verifyECDSASig(sender, data, signature);
     }
 
-    // Returns true if new transaction could be created.
+    /**
+     * Processes the transaction:
+     * 1. Verifies signature
+     * 2. Gathers inputs
+     * 3. Checks sufficient funds
+     * 4. Creates outputs
+     * 
+     * @return true if transaction processed successfully
+     */
     public boolean processTransaction() {
-
-        if (verifySignature() == false) {
+        // First verify signature
+        if (!verifySignature()) {
             System.out.println("#Transaction Signature failed to verify");
             return false;
         }
 
-        // gather transaction inputs (Make sure they are unspent):
+        // Gather transaction inputs from global UTXO pool
         for (TransactionInput i : inputs) {
             i.UTXO = NoobChain.UTXOs.get(i.transactionOutputId);
         }
 
-        // check if transaction is valid:
-        if (getInputsValue() < NoobChain.minimumTransaction) {
-            System.out.println("#Transaction Inputs to small: " + getInputsValue());
+        // Check transaction meets minimum amount
+        float inputValue = getInputsValue();
+        if (inputValue < NoobChain.minimumTransaction) {
+            System.out.println("#Transaction Inputs too small: " + inputValue);
             return false;
         }
 
-        // generate transaction outputs:
-        float leftOver = getInputsValue() - value; // get value of inputs then the left over change:
-        transactionId = calulateHash();
-        outputs.add(new TransactionOutput(this.recipient, value, transactionId)); // send value to recipient
+        // Calculate leftover change
+        float leftOver = inputValue - value;
+        transactionId = calculateHash();
+
+        // Create outputs:
+        // 1. Payment to recipient
+        outputs.add(new TransactionOutput(this.recipient, value, transactionId));
+
+        // 2. Change back to sender (if any)
         if (leftOver > 0) {
-            outputs.add(new TransactionOutput(this.sender, leftOver, transactionId)); // send the left over 'change'
-                                                                                      // back to
-            // sender
+            outputs.add(new TransactionOutput(this.sender, leftOver, transactionId));
         }
 
-        // add outputs to Unspent list
+        // Add outputs to global UTXO pool
         for (TransactionOutput o : outputs) {
             NoobChain.UTXOs.put(o.id, o);
         }
 
-        // remove transaction inputs from UTXO lists as spent:
+        // Remove spent inputs from UTXO pool
         for (TransactionInput i : inputs) {
-            if (i.UTXO == null)
-                continue; // if Transaction can't be found skip it
-            NoobChain.UTXOs.remove(i.UTXO.id);
+            if (i.UTXO != null) {
+                NoobChain.UTXOs.remove(i.UTXO.id);
+            }
         }
 
         return true;
     }
 
-    // returns sum of inputs(UTXOs) values
+    /**
+     * Calculates total value of inputs
+     * 
+     * @return Sum of all input values
+     */
     public float getInputsValue() {
         float total = 0;
         for (TransactionInput i : inputs) {
-            if (i.UTXO == null)
-                continue; // if Transaction can't be found skip it
-            total += i.UTXO.value;
+            if (i.UTXO != null) {
+                total += i.UTXO.value;
+            }
         }
         return total;
     }
 
-    // returns sum of outputs:
+    /**
+     * Calculates total value of outputs
+     * 
+     * @return Sum of all output values
+     */
     public float getOutputsValue() {
         float total = 0;
         for (TransactionOutput o : outputs) {
@@ -108,5 +160,4 @@ public class Transaction {
         }
         return total;
     }
-
 }
