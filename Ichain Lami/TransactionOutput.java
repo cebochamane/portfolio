b32496826589e17255a604c9a9b04d3f49b3f98a
cebochamane <cebosbongachamane@gmail.com>
2025-06-25 04:55:06 +0200

    Updated TransactionOutput.java

--- Ichain Lami/TransactionOutput.java
+++ Ichain Lami/TransactionOutput.java
@@ -1,24 +1,49 @@
 import java.security.PublicKey;
 
+/**
+ * Represents an unspent transaction output (UTXO)
+ * Can be spent as input to new transactions
+ */
 public class TransactionOutput {
-    public String id;
-    public PublicKey reciepient; // also known as the new owner of these coins.
-    public float value; // the amount of coins they own
-    public String parentTransactionId; // the id of the transaction this output was created in
 
-    // Constructor
-    public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
-        this.reciepient = reciepient;
+    // Unique identifier for this output
+    public String id;
+
+    // Recipient's public key (who can spend this output)
+    public PublicKey recipient;
+
+    // Value of this output
+    public float value;
+
+    // ID of transaction that created this output
+    public String parentTransactionId;
+
+    /**
+     * Constructor
+     * 
+     * @param recipient           Owner of this output
+     * @param value               Amount this output represents
+     * @param parentTransactionId Transaction that created this output
+     */
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+        this.recipient = recipient;
         this.value = value;
         this.parentTransactionId = parentTransactionId;
-        this.id = StringUtil
-                .turnIntoUnrecognizableGibberish(
-                        StringUtil.getStringFromKey(reciepient) + Float.toString(value) + parentTransactionId);
+
+        // Create ID by hashing recipient, value and parent transaction
+        this.id = StringUtil.applySha256(
+                StringUtil.getStringFromKey(recipient) +
+                        Float.toString(value) +
+                        parentTransactionId);
     }
 
-    // Check if coin belongs to you
+    /**
+     * Checks if this output belongs to specified public key
+     * 
+     * @param publicKey Key to check ownership against
+     * @return true if output belongs to this key
+     */
     public boolean isMine(PublicKey publicKey) {
-        return (publicKey == reciepient);
+        return recipient.equals(publicKey);
     }
-
 }
